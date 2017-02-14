b81b87f911d6386bf4924b421f91b35d23b5fdbd
Raj Singh <anilraj82@yahoo.com>
2017-02-14 15:15:41 +0200

    apps created

--- Material/src/app/app.module.ts
+++ Material/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageTwoComponent } from './page-two/page-two.component';
+import { SettingComponent } from './setting/setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageOneComponent,
+    PageTwoComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
